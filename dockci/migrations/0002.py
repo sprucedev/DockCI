d58a85b922d2159b16bc16be46b5c09175567ece
Ricky Cook <mail@thatpanda.com>
2015-03-08 21:14:58 +1100

    Handle no config.yaml in migrations

--- dockci/migrations/0002.py
+++ dockci/migrations/0002.py
@@ -2,12 +2,17 @@
 Migrate config to docker hosts list
 """
 import os
+import sys
 import yaml
 
 
 filename = os.path.join('data', 'configs.yaml')
-with open(filename) as handle:
-    data = yaml.load(handle)
+try:
+    with open(filename) as handle:
+        data = yaml.load(handle)
+except FileNotFoundError:
+    # This is fine; will fail for new installs
+    sys.exit(0)
 
 host = data.pop('docker_host')
 data['docker_hosts'] = [host]
