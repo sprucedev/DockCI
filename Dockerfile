8ecc642ac0f9fe69a71cc97495251654bc4d14b1
Ricky Cook <mail@thatpanda.com>
2015-08-05 23:28:47 +1000

    Update quite a number of things for python wheels

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,7 @@ ENTRYPOINT ["/usr/bin/make"]
 CMD ["run"]
 
 ENV DEBIAN_FRONTEND noninteractive
-RUN apt-get update && apt-get install -y aria2 && \
-    aria2c -d /tmp "https://raw.githubusercontent.com/RickyCook/minimal-apt-fast/5750510/install.sh" && \
-    APT_FAST_VERSION=v1.7.6 NO_APT_UPDATE=y sh -e /tmp/install.sh && \
-    rm /tmp/install.sh && \
-    apt-fast install -y \
+RUN apt-get update && apt-get install -y
         git nodejs npm locales \
         python3 python3-pip python3-virtualenv
 RUN ln -s $(which nodejs) /usr/bin/node
@@ -23,6 +19,7 @@ ADD Makefile /code/Makefile
 
 ADD package.json /code/package.json
 ADD bower.json /code/bower.json
+ADD _collect_static.sh /code/_collect_static.sh
 RUN make htmldeps collectstatic
 
 ADD requirements.txt /code/requirements.txt
---
+++ _collect_static.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cp bower_components/bootstrap/dist/css/bootstrap.min.css dockci/static/lib/css
+cp bower_components/bootstrap-material-design/dist/css/material.min.css dockci/static/lib/css
+cp bower_components/bootstrap-material-design/dist/css/material-wfont.min.css dockci/static/lib/css
+cp bower_components/bootstrap-material-design/dist/css/ripples.min.css dockci/static/lib/css
+
+cp bower_components/bootstrap-material-design/dist/fonts/Material-Design-Icons.woff dockci/static/lib/fonts
+cp bower_components/bootstrap-material-design/dist/fonts/Material-Design-Icons.ttf dockci/static/lib/fonts
+
+cp bower_components/ansi_up/ansi_up.js dockci/static/lib/js
+cp bower_components/blueimp-md5/js/md5.min.js dockci/static/lib/js
+cp bower_components/bootstrap/js/dropdown.js dockci/static/lib/js
+cp bower_components/bootstrap/js/modal.js dockci/static/lib/js
+cp bower_components/bootstrap/js/tab.js dockci/static/lib/js
+cp bower_components/bootstrap/js/transition.js dockci/static/lib/js
+cp bower_components/bootstrap-material-design/dist/js/material.min.js dockci/static/lib/js
+cp bower_components/bootstrap-material-design/dist/js/ripples.min.js dockci/static/lib/js
+cp bower_components/jquery/dist/jquery.min.js dockci/static/lib/js
+cp bower_components/uri.js/src/jquery.URI.min.js dockci/static/lib/js
+cp bower_components/uri.js/src/URI.min.js dockci/static/lib/js
