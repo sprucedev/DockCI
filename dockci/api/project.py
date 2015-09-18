8644065c2adec545453c0c7835da8e02d028383e
Ricky Cook <mail@thatpanda.com>
2015-09-18 12:36:15 +1000

    Change slug to project_slug in api

--- dockci/api/project.py
+++ dockci/api/project.py
@@ -3,7 +3,7 @@ from flask_restful import fields, marshal_with, Resource
 from flask_security import login_required
 
 from .base import BaseDetailResource, BaseRequestParser
-from .util import new_edit_parsers
+from .util import new_edit_parsers, RewriteUrl
 from dockci.models.project import Project
 from dockci.server import API, DB
 
@@ -20,7 +20,7 @@ BASIC_FIELDS = {
 
 
 LIST_FIELDS = {
-    'detail': fields.Url('project_detail'),
+    'detail': RewriteUrl('project_detail', rewrites=dict(project_slug='slug')),
 }
 LIST_FIELDS.update(BASIC_FIELDS)
 
@@ -63,19 +63,19 @@ class ProjectList(Resource):
 
 class ProjectDetail(BaseDetailResource):
     @marshal_with(DETAIL_FIELDS)
-    def get(self, slug):
-        return Project.query.filter_by(slug=slug).first_or_404()
+    def get(self, project_slug):
+        return Project.query.filter_by(slug=project_slug).first_or_404()
 
     @login_required
     @marshal_with(DETAIL_FIELDS)
-    def put(self, slug):
+    def put(self, project_slug):
         project = Project()
         return self.handle_write(project, PROJECT_NEW_PARSER)
 
     @login_required
     @marshal_with(DETAIL_FIELDS)
-    def post(self, slug):
-        project = Project.query.filter_by(slug=slug).first_or_404()
+    def post(self, project_slug):
+        project = Project.query.filter_by(slug=project_slug).first_or_404()
         return self.handle_write(project, PROJECT_EDIT_PARSER)
 
 
@@ -83,5 +83,5 @@ API.add_resource(ProjectList,
                  '/projects',
                  endpoint='project_list')
 API.add_resource(ProjectDetail,
-                 '/projects/<string:slug>',
+                 '/projects/<string:project_slug>',
                  endpoint='project_detail')
