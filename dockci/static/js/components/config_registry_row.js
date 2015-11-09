78271e075a55b5985d2782ab6ac110cf31edfdd6
Ricky Cook <ricky@spruce.sh>
2015-11-09 23:25:43 +1100

    Front end registry save new and delete

--- dockci/static/js/components/config_registry_row.js
+++ dockci/static/js/components/config_registry_row.js
@@ -1,25 +1,52 @@
 define([
     'knockout'
   , '../util'
+  , '../models/message'
   , 'text!./config_registry_row.html'
-], function(ko, util, template) {
+], function(ko, util, MessageModel, template) {
   function ConfigRegistryRowModel(params) {
     finalParams = $.extend({
-      'editMode': false
+        'editMode': false
+      , 'messages': []
     }, params)
 
     this.saving = ko.observable(false)
+    this.messages = util.paramArray(finalParams['messages'])
 
     this.registry = util.param(finalParams['registry'])
     this.editMode = util.param(finalParams['editMode'])
 
+    this.savable = ko.computed(function() {
+      var   displayName = this.registry().display_name()
+          , baseName = this.registry().base_name()
+          , username = this.registry().username()
+
+      return    displayName !== null && displayName !== ''
+             && baseName !== null && baseName !== ''
+             && username !== null && username !== ''
+    }.bind(this))
+
     this.save = function() {
       this.saving(true)
-      setTimeout(function() { this.saving(false) }.bind(this), 1000)
+      this.registry().save().success(function() {
+        this.messages([new MessageModel({
+            'message': "Saved registry"
+          , 'category': 'success'
+        })])
+      }.bind(this)).always(function() {
+        this.saving(false)
+      }.bind(this)).fail(util.ajax_fail(this.messages))
     }.bind(this)
     this.remove = function() {
       this.saving(true)
-      setTimeout(function() { this.saving(false) }.bind(this), 1000)
+      this.registry().remove().success(function() {
+        this.messages([new MessageModel({
+            'message': "Removed registry"
+          , 'category': 'success'
+        })])
+      }.bind(this)).always(function() {
+        this.saving(false)
+      }.bind(this)).fail(util.ajax_fail(this.messages))
     }.bind(this)
   }
 
--- dockci/static/js/models/registry.js
+++ dockci/static/js/models/registry.js
@@ -14,7 +14,36 @@ define(['knockout', 'app/util'], function (ko, util) {
     this.username = util.param(finalParams['username'])
     this.password = util.param(finalParams['password'])
     this.email = util.param(finalParams['email'])
-    this.detail = util.params(finalParams['detail'])
+    this.raw_detail = util.param(finalParams['detail'])
+
+    this.isNew = ko.computed(function() { return this.raw_detail() === null }.bind(this))
+
+    this.detail = ko.computed(function() {
+      var raw_detail = this.raw_detail()
+      return raw_detail !== null ?
+        raw_detail :
+        '/api/v1/registries/' + encodeURIComponent(this.base_name())
+    }.bind(this))
+
+    this.forApi = function() {
+      return {
+          'display_name': this.display_name() || undefined
+        , 'username': this.username() || undefined
+        , 'password': this.password() || undefined
+        , 'email': this.email() || undefined
+      }
+    }.bind(this)
+
+    this.save = function() {
+      return $.ajax(this.detail(), {
+          'method': this.isNew() ? 'PUT' : 'POST'
+        , 'data': this.forApi()
+        , 'dataType': 'json'
+      })
+    }.bind(this)
+    this.remove = function() {
+      return $.ajax(this.detail(), {'method': 'DELETE'})
+    }.bind(this)
   }
   return RegistryModel
 })
