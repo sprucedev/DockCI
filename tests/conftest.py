1a57074c01b75649a8b99985081b500a730fc831
Ricky Cook <ricky@spruce.sh>
2016-01-02 23:19:22 +1100

    DB fixtures for test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import subprocess
 
+from urllib.parse import urlparse, urlunparse
+
+import alembic
 import pytest
+
+from flask_migrate import migrate
+
+from dockci.server import APP, app_init, DB, MIGRATE
 
 
 @pytest.yield_fixture
@@ -15,3 +22,44 @@ def tmpgitdir(tmpdir):
             subprocess.check_call(['git', 'config', name, val])
 
         yield tmpdir
+
+
+@pytest.yield_fixture(scope='session')
+def base_db_conn():
+    """ App setup with DockCI database """
+    app_init()
+    try:
+        DB.engine
+
+    except Exception as ex:
+        pytest.skip(str(ex))
+
+    else:
+        db_name = '%s_test' % DB.engine.url.database
+
+        with DB.engine.contextual_connect() as conn:
+            conn.execution_options(isolation_level='AUTOCOMMIT')
+            conn.execute('DROP DATABASE IF EXISTS %s' % db_name)
+            conn.execute('CREATE DATABASE %s' % db_name)
+
+            DB.engine.url.database = db_name
+            APP.config['SQLALCHEMY_DATABASE_URI'] = DB.engine.url
+            app_init()
+
+            yield conn
+
+            conn.execute('DROP DATABASE %s' % db_name)
+
+
+@pytest.yield_fixture
+def db(base_db_conn):
+    """ App setup with blank DockCI database """
+    config = MIGRATE.get_config(None)
+    alembic.command.upgrade(config, 'head')
+
+    session = DB.session
+    session.begin_nested()
+    try:
+        yield
+    finally:
+        session.rollback()
