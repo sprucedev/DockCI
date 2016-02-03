80985f88b7a19e6a73e97e13cff78302150e2304
Ricky Cook <ricky@spruce.sh>
2016-02-03 16:30:19 +1100

    Revert "Remove accidental subunit changes"
    This reverts commit a481db3efb78df3d79664e8f085dbb4e452499fb.

--- dockci/models/job_meta/stages_main.py
+++ dockci/models/job_meta/stages_main.py
@@ -4,6 +4,8 @@ Main job stages that constitute a job
 
 import json
 
+from subunit.v2 import ByteStreamToStreamResult
+
 from dockci.models.base import ServiceBase
 from dockci.models.job_meta.stages import JobStageBase, DockerStage
 from dockci.util import built_docker_image_id
@@ -161,7 +163,7 @@ class BuildStage(DockerStage):
         return 1
 
 
-class TestStage(DockerStage):
+class TestStage(JobStageBase):
     """
     Tell the Docker host to run the CI command
     """
@@ -174,19 +176,13 @@ class TestStage(DockerStage):
         ``DockerStage`` runnable to execute Docker-based tests
         """
         if self.job.job_config.skip_tests:
-            handle.write("Skipping tests, as per configuration".encode())
+            # todo output subunit
+            #handle.write("Skipping tests, as per configuration".encode())
             self.job.exit_code = 0
             self.job.db_session.add(self.job)
             self.job.db_session.commit()
             return 0
 
-        return super(TestStage, self).runnable(handle)
-
-    def runnable_docker(self):
-        """
-        Create a container instance, attach to its outputs and then start it,
-        returning the output stream
-        """
         container_details = self.job.docker_client.create_container(
             self.job.image_id, 'ci'
         )
@@ -230,7 +226,13 @@ class TestStage(DockerStage):
             ]
         )
 
-        return stream
+        result = ByteStreamToStreamResult(stream, non_subunit_name='otherthings')
+        class DoIt(object):
+            def status(self, **kwargs):
+                handle.write(json.dumps(kwargs).encode())
+                handle.write(b'\n')
+
+        result.run(DoIt())
 
     def on_done(self, _):
         """
