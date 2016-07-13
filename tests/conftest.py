67f681c10638dcf9174cf7a3f781d9d5fc39aeaa
Ricky Cook <ricky@spruce.sh>
2016-07-13 21:00:58 +1000

    Connections remain after test, so don't drop db

--- tests/conftest.py
+++ tests/conftest.py
@@ -51,10 +51,7 @@ def base_db_conn():
             APP.config['SQLALCHEMY_DATABASE_URI'] = DB.engine.url
             app_init()
 
-            try:
-                yield conn
-            finally:
-                conn.execute('DROP DATABASE %s' % db_name)
+            yield conn
 
 
 @pytest.yield_fixture
