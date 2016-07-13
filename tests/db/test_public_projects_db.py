4f793f5075a2e27be96c0ca9572453dc04d3370f
Ricky Cook <ricky@spruce.sh>
2016-07-13 19:29:54 +1000

    Better tests

--- tests/db/test_public_projects_db.py
+++ tests/db/test_public_projects_db.py
@@ -4,74 +4,125 @@ import json
 import pytest
 
 from dockci.models.project import Project
+from dockci.models.job import Job, JobStageTmp
 from dockci.server import DB
 
 
-@pytest.fixture(scope='module')
-def projects(request):
+def model_helper(request, models):
+    for model in models:
+        DB.session.add(model)
+    DB.session.commit()
+
+    def fin():
+        for model in models:
+            DB.session.delete(model)
+        DB.session.commit()
+    request.addfinalizer(fin)
+
+    return models
+
+
+def clean():
+    JobStageTmp.query.delete()
+    Job.query.delete()
+    Project.query.delete()
+    DB.session.commit()
+
+
+@pytest.fixture
+def projects(request, db):
+    clean()
     vals = [
         Project(slug='pp-pub1', repo='', name='', utility=False, public=True),
         Project(slug='pp-pri1', repo='', name='', utility=False, public=False),
         Project(slug='pp-pub2', repo='', name='', utility=False, public=True),
         Project(slug='pp-pri2', repo='', name='', utility=False, public=False),
     ]
-
-    for proj in vals:
-        proj_ex = Project.query.filter_by(slug=proj.slug).first()
-        if proj_ex:
-            DB.session.delete(proj_ex)
-    DB.session.commit()
-
-    for proj in vals:
-        DB.session.add(proj)
-    DB.session.commit()
-
-    def fin():
-        for proj in vals:
-            DB.session.delete(proj)
-        DB.session.commit()
-    request.addfinalizer(fin)
-
-    return vals
+    return model_helper(request, vals)
 
 
-@pytest.mark.usefixtures('db')
-@pytest.mark.usefixtures('projects')
+@pytest.fixture
+def jobs(request, projects, db):
+    vals = [
+        Job(project=project, commit='test', repo_fs='')
+        for project in projects
+    ]
+    return model_helper(request, vals)
+
+
+@pytest.fixture
+def stages(request, jobs, db):
+    vals = [
+        JobStageTmp(job=job, slug='test')
+        for job in jobs
+    ]
+    return model_helper(request, vals)
+
+
 class TestPublicProjects(object):
+    @pytest.mark.usefixtures('stages')
     @pytest.mark.parametrize('url_prefix', ['', '/api/v1'])
-    @pytest.mark.parametrize('url,exp_status', [
-        ('/projects/pp-pub1', 200),
-        ('/projects/pp-pri1', 404),
-        ('/projects/pp-pub2', 200),
-        ('/projects/pp-pri2', 404),
+    @pytest.mark.parametrize('url_fs', [
+        '/projects/{project_slug}',
+        '/projects/{project_slug}/jobs/{job_slug}',
+        '/projects/{project_slug}/jobs/{job_slug}/stages/{stage_slug}',
     ])
-    def test_project_guest(self,
+    @pytest.mark.parametrize('project_slug,exp_status', [
+        ('pp-pub1', 200),
+        ('pp-pri1', 404),
+        ('pp-pub2', 200),
+        ('pp-pri2', 404),
+    ])
+    def test_guest(self,
                            client,
+                           jobs,
                            url_prefix,
-                           url,
+                           url_fs,
+                           project_slug,
                            exp_status,
                            ):
         """ Ensure only public projects accessible as guest """
-        full_url = '%s%s' % (url_prefix, url)
+        project = Project.query.filter_by(slug=project_slug)[0]
+        job = project.jobs[0]
+        stage = job.job_stages[0]
+
+        full_url = ('%s%s' % (url_prefix, url_fs)).format(
+            project_slug=project.slug,
+            job_slug=job.slug,
+            stage_slug=stage.slug,
+        )
         response = client.get(full_url)
 
         assert response.status_code == exp_status
 
+    @pytest.mark.usefixtures('stages')
     @pytest.mark.parametrize('url_prefix', ['', '/api/v1'])
-    @pytest.mark.parametrize('url', [
-        '/projects/pp-pub1',
-        '/projects/pp-pri1',
-        '/projects/pp-pub2',
-        '/projects/pp-pri2',
+    @pytest.mark.parametrize('url_fs', [
+        '/projects/{project_slug}',
+        '/projects/{project_slug}/jobs/{job_slug}',
+        '/projects/{project_slug}/jobs/{job_slug}/stages/{stage_slug}',
     ])
-    def test_project_user(self,
-                          client,
-                          user,
-                          url_prefix,
-                          url,
-                          ):
-        """ Ensure all projects accessible as user """
-        full_url = '%s%s' % (url_prefix, url)
+    @pytest.mark.parametrize('project_slug', [
+        'pp-pub1', 'pp-pri1', 'pp-pub2', 'pp-pri2',
+    ])
+    def test_user(self,
+                           client,
+                           jobs,
+                           user,
+                           url_prefix,
+                           url_fs,
+                           project_slug,
+                           ):
+        """ Ensure only public projects accessible as guest """
+        project = Project.query.filter_by(slug=project_slug)[0]
+        job = project.jobs[0]
+        stage = job.job_stages[0]
+
+        full_url = ('%s%s' % (url_prefix, url_fs)).format(
+            project_slug=project.slug,
+            job_slug=job.slug,
+            stage_slug=stage.slug,
+        )
         response = client.get(full_url, headers={
             'x_dockci_username': user.email,
             'x_dockci_password': 'testpass',
